116618a73335b3d7bb09ab5dc9dddac415dfa301
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-12-26 00:03:48 +0430

    new files added

--- Logout.php
+++ Logout.php
@@ -6,6 +6,7 @@ unset($_SESSION['Change_Role_ID']);
 unset($_SESSION['IS_LOGIN']);
 
 unset($_SESSION['Change_Full_Name']);
+
 unset($_SESSION['Cahange_User_ID']);
 
 unset($_SESSION['Change_Username']);
