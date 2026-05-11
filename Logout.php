2839576ef172419a156cf37288a34e49d4e439e5
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-05-11 10:03:14 +0430

    new changes

--- Logout.php
+++ Logout.php
@@ -4,7 +4,6 @@ session_start();
 unset($_SESSION['Change_Role_ID']);
 
 unset($_SESSION['IS_LOGIN']);
-
 unset($_SESSION['Change_Full_Name']);
 unset($_SESSION['Cahange_User_ID']);
 unset($_SESSION['Change_Username']);
